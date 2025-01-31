7dd9dd2e1b767e9aec9804e732c14f3471c3013d
kavyakapoor420 <kavyakapoor412@gmail.com>
2025-01-31 18:25:32 +0530

    added cors to connect with frontend

--- OsdagBackend/OsdagBackend/settings.py
+++ OsdagBackend/OsdagBackend/settings.py
@@ -37,13 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Myapp'
+    'Myapp',
+    'corsheaders'
+
 ]
-
-
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # cors 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'OsdagBackend.urls'
 
@@ -124,3 +125,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",  # the  frontend URL of react server 
+# ]
+
+CORS_ALLOW_ALL_ORIGINS=True
+CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+CORS_ALLOW_HEADERS = ["*"]
